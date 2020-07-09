6c5579af9b62447ee7d060c2d352b459691f7c18
vismith24 <vismith.24.adappa@gmail.com>
2020-07-09 10:34:34 +0530

    July Day 1

---
+++ July/Day1_ArrangingCoins.py
@@ -0,0 +1,16 @@
+'''
+You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+
+Given n, find the total number of full staircase rows that can be formed.
+
+n is a non-negative integer and fits within the range of a 32-bit signed integer.
+'''
+
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        k = (-1 + (1 + 8*n)**(1/2))/2
+        
+        if k.is_integer():
+            return int(k)
+        else:
+            return int(k)
